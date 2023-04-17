d4329580b5ade21a6689b179bd0f1f282a03f3a3
JamesRJGray <jamesrjgray@gmail.com>
2023-04-17 18:14:02 -0500

    added test route to usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const { usersCtrl } = require('../controllers') // all methods
 
 // ROUTES - METHODS //
+router.get('/')
 router.get('/api/users/:id', usersCtrl.getUser) // find user
 router.post('/api/users/register', usersCtrl.createUser) // Create user
 router.delete('/api/users/:id', usersCtrl.deleteUser) // delete user
