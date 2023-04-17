6a6d016208165628f49ef31d86412fff7bcdd2c5
JamesRJGray <jamesrjgray@gmail.com>
2023-04-17 18:31:27 -0500

    testing route update for deployment

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,7 +3,7 @@ const { usersCtrl } = require('../controllers') // all methods
 
 // ROUTES - METHODS //
 router.get('/')
-router.get('/api/users/:id', usersCtrl.getUser) // find user
+// router.get('/api/users/:id', usersCtrl.getUser) // find user
 router.post('/api/users/register', usersCtrl.createUser) // Create user
 router.delete('/api/users/:id', usersCtrl.deleteUser) // delete user
 
